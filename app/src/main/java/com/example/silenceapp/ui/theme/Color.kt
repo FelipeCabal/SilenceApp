48f36692be8e9b85ee7036fc2a8d985d395775f6
Felipe Cabal <phelipekkabal_85@hotmail.com>
2025-10-02 20:24:49 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/silenceapp/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.silenceapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MediumGreen = Color(0xFF64C27B)
+val DarkGreen   = Color(0xFF2A8C4A)
+val PaleMint    = Color(0xFFD0FDD7)
+val MintGreen   = Color(0xFF9BFAB0)
+
+val VeryLightGreen = Color(0xFFFBFFF8)
+
+val DarkGray = Color(0xFF1F1F1F)
+val DimGray = Color(0xFF404040)
+val White = Color(0xFFFFFFFF)
+val MintWhite = Color(0xFFFBFFF8)
+
+
+
---
+++ app/src/main/java/com/example/silenceapp/ui/theme/ThemeExtensions.kt
@@ -0,0 +1,20 @@
+package com.example.silenceapp.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val primaryColor: Color
+    @Composable get() = MaterialTheme.colorScheme.primary
+
+val onPrimaryColor: Color
+    @Composable get() = MaterialTheme.colorScheme.onPrimary
+
+val secondaryColor: Color
+    @Composable get() = MaterialTheme.colorScheme.secondary
+
+val backgroundColor: Color
+    @Composable get() = MaterialTheme.colorScheme.background
+
+val surfaceColor: Color
+    @Composable get() = MaterialTheme.colorScheme.surface
